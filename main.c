76ee77a0a6e42752bbbaa6d41bdf4e0947432982
ehteshamtarq <ehteshamtarique1@gmail.com>
2024-08-31 13:24:39 +0530

    Disassembler Implementation
    Encoding and Decoding of the bytecode implemented

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include "common.h"
-int main(int argc, const char* argv[]) {
-return 0;
+#include "chunk.h"
+#include "debug.h"
+
+int main(int argc, const char *argv[])
+{
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+    return 0;
 }
--- memory.c
+++ memory.c
@@ -10,6 +10,7 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize)
         return NULL;
     }
     void *result = realloc(pointer, newSize);
-    if (result == NULL) exit(1);
+    if (result == NULL)
+        exit(1);
     return result;
 }
