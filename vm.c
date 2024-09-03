74533f70f56e0b42fde5567ed8c29bcc86454412
ehteshamtarq <ehteshamtarique1@gmail.com>
2024-09-03 12:40:00 +0530

    Stack Implementation in the VM

--- vm.c
+++ vm.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
+
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
 
 VM vm;
 
+static void resetStack()
+{
+    vm.stackTop = vm.stack;
+}
+
 void initVM()
 {
+    resetStack();
 }
+
 void freeVM()
 {
+}
+
+void push(Value value)
+{
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop()
+{
+    vm.stackTop--;
+    return *vm.stackTop;
 }
 
 static InterpretResult run()
@@ -17,18 +38,31 @@ static InterpretResult run()
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
     for (;;)
     {
+#ifdef DEBUG_TRACE_EXECUTION
+        disassembleInstruction(vm.chunk,
+                               (int)(vm.ip - vm.chunk->code));
+        printf("    ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+        {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+#endif
         uint8_t instruction;
         switch (instruction = READ_BYTE())
         {
         case OP_CONSTANT:
         {
             Value constant = READ_CONSTANT();
-            printValue(constant);
-            printf("\n");
+            push(constant);
             break;
         }
         case OP_RETURN:
         {
+            printValue(pop());
+            printf("\n");
             return INTERPRET_OK;
         }
         }
--- vm.h
+++ vm.h
@@ -2,11 +2,16 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct
 {
     Chunk *chunk; // The chunk of bytecode to execute.
-    uint8_t* ip; // The instruction pointer (IP).
+    uint8_t *ip;  // The instruction pointer (IP).
+    Value stack[STACK_MAX];  // Stack Implementation in the VM
+    Value *stackTop;   // To keep track of the top of the stack
 } VM;
 
 typedef enum
@@ -19,5 +24,7 @@ typedef enum
 void initVM();
 void freeVM();
 InterpretResult interpret(Chunk *chunk);
+void push(Value value);
+Value pop();
 
 #endif
