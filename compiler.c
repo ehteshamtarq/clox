e7a3dbad2f3ab48763eaa8558ef5e407810c038e
ehteshamtarq <ehteshamtarique1@gmail.com>
2024-09-06 01:27:41 +0530

    EMITTING BYTECODE

--- compiler.c
+++ compiler.c
@@ -16,6 +16,12 @@ typedef struct
 
 Parser parser;
 
+Chunk *compilingChunk;
+static Chunk *currentChunk()
+{
+    return compilingChunk;
+}
+
 static void errorAt(Token *token, const char *message)
 {
     if (parser.panicMode)
@@ -71,6 +77,27 @@ static void consume(TokenType type, const char *message)
     errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte)
+{
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void endCompiler()
+{
+    emitReturn();
+}
+
+static void emitReturn()
+{
+    emitByte(OP_RETURN);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+    emitByte(byte1);
+    emitByte(byte2);
+}   
+
 /*The first column is the line number, the second is the numeric value of the
 token type, and then finally the lexeme. That last empty lexeme on line 2 is the
 EOF token.*/
@@ -78,6 +105,7 @@ EOF token.*/
 bool compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
+    compilingChunk = chunk;
 
     parser.hadError = false;
     parser.panicMode = false;
@@ -85,6 +113,7 @@ bool compile(const char *source, Chunk *chunk)
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
     return !parser.hadError;
     int line = -1;
     for (;;)
